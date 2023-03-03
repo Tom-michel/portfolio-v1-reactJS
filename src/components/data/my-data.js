bbce5c01714bbb8d081b3cd2c792253ed66f12b7
Tom-michel <rufin.btompe@facsciences-uy1.cm>
2023-03-03 16:17:34 +0100

    Filling data

---
+++ src/components/data/my-data.js
@@ -0,0 +1,302 @@
+export const brandlink = [
+  {
+    url: "#home",
+    img: "../images/logo.png",
+  },
+];
+
+export const navlink = [
+  {
+    url: "#about",
+    text: "About",
+  },
+  {
+    url: "#skills",
+    text: "Skills",
+  },
+  {
+    url: "#projects",
+    text: "Projects",
+  },
+];
+
+export const contactlink = [
+  {
+    url: "...",
+    text: "LinkedIn",
+    icon: "../images/tech/...",
+  },
+  {
+    url: "...",
+    text: "GitHub",
+    icon: "../images/tech/...",
+  },
+  {
+    url: "...",
+    text: "GitLab",
+    icon: "../images/tech/...",
+  },
+
+  {
+    url: "...",
+    text: "ContactMe",
+    icon: "../images/tech/...",
+  },
+];
+
+export const banner = [
+  {
+    text: "Hello, i am",
+    name: "Michel Btompe",
+    role: ["Full Stack Developer", "UI/UX Designer"],
+    exper_num: "5",
+    project_num: "10+",
+    img: "../images/...",
+    img_tech1: "../images/...",
+    img_tech2: "../images/...",
+    img_tech3: "../images/...",
+  },
+];
+
+export const about = [
+  {
+    text: "...",
+    user: "@Tom",
+    role: "...",
+  },
+];
+
+export const skills = [
+  {
+    name: "JavaScript",
+    img: "../images/...",
+    dots: "...",
+    starred: false,
+  },
+  {
+    name: "TypeScript",
+    img: "../images/...",
+    dots: "...",
+    starred: false,
+  },
+  {
+    name: "HTML5",
+    img: "../images/...",
+    dots: "...",
+    starred: false,
+  },
+  {
+    name: "CSS3",
+    img: "../images/...",
+    dots: "...",
+    starred: false,
+  },
+  {
+    name: "ReactJS",
+    img: "../images/...",
+    dots: "...",
+    starred: false,
+  },
+  {
+    name: "Flutter",
+    img: "../images/...",
+    dots: "...",
+    starred: true,
+  },
+  {
+    name: "GitHub",
+    img: "../images/...",
+    dots: "...",
+    starred: false,
+  },
+  {
+    name: "GitLab",
+    img: "../images/...",
+    dots: "...",
+    starred: false,
+  },
+  {
+    name: "Django",
+    img: "../images/...",
+    dots: "...",
+    starred: true,
+  },
+  {
+    name: "Python",
+    img: "../images/...",
+    dots: "...",
+    starred: false,
+  },
+  {
+    name: "TensorFlow",
+    img: "../images/...",
+    dots: "...",
+    starred: false,
+  },
+  {
+    name: "MySQL",
+    img: "../images/...",
+    dots: "...",
+    starred: true,
+  },
+  {
+    name: "MongoDB",
+    img: "../images/...",
+    dots: "...",
+    starred: false,
+  },
+];
+
+export const project = [
+  {
+    name: "CNIC",
+    img: "../images/...",
+    desc: "...",
+    date: "...",
+    tech: ["...", "...", "..."],
+    tags: ["...", "...", "..."],
+    category: "mobile",
+  },
+  {
+    name: "Wassup",
+    img: "../images/...",
+    desc: "...",
+    date: "...",
+    tech: ["...", "...", "..."],
+    tags: ["...", "...", "..."],
+    category: "mobile",
+  },
+  {
+    name: "Wassup Design",
+    img: "../images/...",
+    desc: "...",
+    date: "...",
+    tech: ["...", "...", "..."],
+    tags: ["...", "...", "..."],
+    category: "design",
+  },
+  {
+    name: "Food Agenda",
+    img: "../images/...",
+    desc: "...",
+    date: "...",
+    tech: ["...", "...", "..."],
+    tags: ["...", "...", "..."],
+    category: "mobile",
+  },
+  {
+    name: "Food Recognition",
+    img: "../images/...",
+    desc: "...",
+    date: "...",
+    tech: ["...", "...", "..."],
+    tags: ["...", "...", "..."],
+    category: "mobile",
+  },
+  {
+    name: "Food Annotation",
+    img: "../images/...",
+    desc: "...",
+    date: "...",
+    tech: ["...", "...", "..."],
+    tags: ["...", "...", "..."],
+    category: "mobile",
+  },
+  {
+    name: "FoodAnnotation Design",
+    img: "../images/...",
+    desc: "...",
+    date: "...",
+    tech: ["...", "...", "..."],
+    tags: ["...", "...", "..."],
+    category: "design",
+  },
+  {
+    name: "DolphPretty",
+    img: "../images/...",
+    desc: "...",
+    date: "...",
+    tech: ["...", "...", "..."],
+    tags: ["...", "...", "..."],
+    category: "web",
+  },
+  {
+    name: "KennerBrige",
+    img: "../images/...",
+    desc: "...",
+    date: "...",
+    tech: ["...", "...", "..."],
+    tags: ["...", "...", "..."],
+    category: "web",
+  },
+  {
+    name: "Jerry",
+    img: "../images/...",
+    desc: "...",
+    date: "...",
+    tech: ["...", "...", "..."],
+    tags: ["...", "...", "..."],
+    category: "web",
+  },
+  {
+    name: "FindInvest",
+    img: "../images/...",
+    desc: "...",
+    date: "...",
+    tech: ["...", "...", "..."],
+    category: "web",
+  },
+  {
+    name: "FindRepater",
+    img: "../images/...",
+    desc: "...",
+    date: "...",
+    tech: ["...", "...", "..."],
+    category: "web",
+  },
+  {
+    name: "Plannig",
+    img: "../images/...",
+    desc: "...",
+    date: "...",
+    tech: ["...", "...", "..."],
+    tags: ["...", "...", "..."],
+    category: "web",
+  },
+  {
+    name: "Umonety",
+    img: "../images/...",
+    desc: "...",
+    date: "...",
+    tech: ["...", "...", "..."],
+    tags: ["...", "...", "..."],
+    category: "web",
+  },
+  {
+    name: "Umonety Design",
+    img: "../images/...",
+    desc: "...",
+    date: "...",
+    tech: ["...", "...", "..."],
+    tags: ["...", "...", "..."],
+    category: "design",
+  },
+];
+
+export const contacts = [
+  {
+    url: "...",
+    text: "WhatsApp",
+    icon: "../images/social/...",
+  },
+  {
+    url: "...",
+    text: "@Michel BTOMPE",
+    icon: "../images/social/...",
+  },
+  {
+    url: "...",
+    text: "@MichelBTOMPE",
+    icon: "../images/social/...",
+  },
+];
