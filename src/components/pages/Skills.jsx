705175bc8d440662588d592702d929d9ca64bb4e
Tom-michel <rufin.btompe@facsciences-uy1.cm>
2023-03-07 22:19:05 +0100

    Bootstrap added in tech

--- src/components/pages/Skills.jsx
+++ src/components/pages/Skills.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Heading } from "../common/Heading";
 import { skills } from "../data/my-data";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineDot from "@mui/lab/TimelineDot";
 
 export const Skills = () => {
   return (
@@ -9,7 +12,15 @@ export const Skills = () => {
         <Heading title="sKills" />
         {skills.map((skill) => (
           <>
-            <p className="description">{skill.text}</p>
+            <p className="description">
+              {skill.text}
+              <TimelineSeparator>
+                <TimelineDot
+                  className="t-gr"
+                  sx={{ backgroundColor: "black" }}
+                />
+              </TimelineSeparator>
+            </p>
             <div className="technos">
               {skill.technos.map((tech) => (
                 <div className="tech"></div>
