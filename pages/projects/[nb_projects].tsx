5acdc05cb6fe542f7d352e83323f071f4887ab37
BTOMPE TCHEUFFA Michel Rufin <michelbtompe@gmail.com>
2023-10-19 21:08:45 +0100

    CREATED with new OS (ub22)

--- pages/projects/[nb_projects].tsx
+++
@@ -1,7 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function NbProjects() {
-	const router = useRouter();
-	const { nb_projects } = router.query;
-	return <div>Projects number: {nb_projects}</div>;
-}
